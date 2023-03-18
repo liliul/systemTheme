3777682448e8531775ec9d79e6396b1f49fe6088
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-03-18 18:09:46 -0300

    feat: sistema de selecionar temas simples

--- src/hook/themes.js
+++ src/hook/themes.js
@@ -1,1 +1,46 @@
-export default console.log('hello world')
+import { guardarNoLocalStorage } from './localStorage.js';
+
+
+const selec = document.querySelector('#select')
+const body = document.querySelector('body')
+
+
+if (localStorage.TempThemes != body.className) {
+ 
+ body.classList.add(localStorage.TempThemes)
+
+   
+  if (localStorage.TempThemes) {
+    let op = document.createElement('option')
+    op.setAttribute('selected','')
+    op.setAttribute('disabled', '')
+    op.textContent = localStorage.TempThemes
+    op.style.color = 'green'
+
+    selec.appendChild(op)
+  }
+}
+  
+
+selec.addEventListener('change', selected);
+
+function selected(e) {
+// console.log('onchange: ', this.value)
+ let isOptionsValue = selec.options[select.selectedIndex];
+
+
+ let isText =  isOptionsValue.text;
+  console.log('isText',isText)
+
+  guardarNoLocalStorage('TempThemes', isText)
+  
+  console.log('selec', select)
+  for (const {text} of selec) {
+    console.log('res: ', text)   
+
+    if (isText === text) {
+      body.className = ''; 
+      body.classList.add(text)     
+    } 
+  }
+}
